5105a5e53599f6013f565ef0e6c3f7bd455c8451
Niraj1903 <niraj.singh1903@outlook.com>
2025-08-05 17:33:12 +0530

    redux installed store created provided store to app appSlice created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Head from "./components/Head";
 import Body from "./components/Body";
+import { Provider } from "react-redux";
+import store from "./utils/store";
 
 function App() {
   return (
     <>
-      <Head />
-      <Body />
+      <Provider store={store}>
+        <Head />
+        <Body />
+      </Provider>
     </>
   );
 }
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,7 +1,15 @@
 const SideBar = () => {
   return (
     <>
-      <div className="">SideBar</div>
+      <div className="shadow-xl w-48">
+        <h1 className="font-bold">Subscriptions</h1>
+        <ul className="">
+          <li className="">Music</li>
+          <li className="">Sports</li>
+          <li className="">Gaming</li>
+          <li className="">Movies</li>
+        </ul>
+      </div>
     </>
   );
 };
