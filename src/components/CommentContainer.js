7dd80138cd170cab189ff9f769ef351d3d3baf30
Niraj1903 <niraj.singh1903@outlook.com>
2025-08-11 23:19:39 +0530

    CommentContainer created

---
+++ src/components/CommentContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CommentContainer = () => {
+  return (
+    <>
+      <div className="mt-2 p-2">
+        <div className="font-bold text-2xl">Comment</div>;
+      </div>
+    </>
+  );
+};
+
+export default CommentContainer;
